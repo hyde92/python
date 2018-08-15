f7e9293a53edb2b64509c8dd555703063ccffa23
hyde92 <32396016+hyde92@users.noreply.github.com>
2018-08-15 15:01:15 +0200

    Delete watchdog.sh

--- watchdog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export ODBCINI=/opt/teradata/client/ODBC_64/odbc.ini
-export ODBCINST=/opt/teradata/client/ODBC_64/odbcinst.ini
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/teradata/client/16.20/odbc_64/lib
-/home/stack/watchdog/watchdog.py> /dev/null 2> /tmp/run_errors/watchdog_err.txt
